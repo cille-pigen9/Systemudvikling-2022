d427d74f7d9f9cdd8557e22b879fc11f059cf7f8
cille-pigen9 <103185635+cille-pigen9@users.noreply.github.com>
2022-04-07 13:21:54 +0200

    Add files via upload

---
+++ Iteration2/hfhf.py
@@ -0,0 +1,15 @@
+from PyQt6.QtWidgets import QApplication, QTableWidget
+
+#database
+
+#person - str
+#navn
+#cpr nr
+
+#Underviser
+#kursus navn
+#Ansættelses nr
+
+#administrator
+#ansættelses nr
+#
