d68032676d494f96566f622de7327447f16aaddb
cille-pigen9 <103185635+cille-pigen9@users.noreply.github.com>
2022-05-29 17:08:27 +0200

    Add files via upload

--- XML/xmlWriter.py
+++ XML/xmlWriter.py
@@ -2,9 +2,46 @@
 <courses>
     <course>
         <courseName>systemudvikling</courseName>
-        <courseId>5100</courseId>
+        <courseId>5100-B3-4F22</courseId>
         <teacher>hugo</teacher>
-        <
-
-
-def __init__(self, courseName, courseId, teacher, studentList, courseDate, classRoom):
+        <studentList>
+            <student>
+                <firstname>cecilie</firstname>
+                <lastname>ludvigsen</lastname>
+                <gender>girl</gender>
+                <id>rsk970</id>
+                <education>sundhed og informatik</education>
+            </student>
+            <student>
+                <firstname>ditte</firstname>
+                <lastname>knudsen</lastname>
+                <gender>girl</gender>
+                <id>pbx732</id>
+                <education>sundhed og informatik</education>
+            </student>
+        </studentList>
+    </course>
+</courses>
+<courses>
+    <course>
+        <courseName>pyhton</courseName>
+        <courseId>5100-B2-2E21</courseId>
+        <teacher>Martin</teacher>
+        <studentList>
+            <student>
+                <firstname>kerim</firstname>
+                <lastname>Gumus</lastname>
+                <gender>boy</gender>
+                <id>zpm472</id>
+                <education>sundhed og informatik</education>
+            </student>
+            <student>
+                <firstname>Sadek</firstname>
+                <lastname>Al-Taai</lastname>
+                <gender>boy</gender>
+                <id>bnm634</id>
+                <education>sundhed og informatik</education>
+            </student>
+        </studentList>
+    </course>
+</courses>
